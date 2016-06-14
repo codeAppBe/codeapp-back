a036816151cb30f44acf09af30a77086a1908c98
Denis Schmitz <schmitzdenis@gmail.com>
2016-06-15 01:12:26 +0200

    connect heroku postgres

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var app = express();
+var pgp = require("pg-promise")(/*options*/);
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -11,8 +12,18 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+var db = pgp("postgres://usgoktrmrqwtmg:BxZd9s90tp6da7Zyjd1Nkp-ok6@ec2-54-228-226-93.eu-west-1.compute.amazonaws.com:5432/ddt5c3cnmvpoqg?ssl=true");
+
 app.get('/', function(request, response) {
-  response.send('Hello From Local NodeJs Server');
+
+  db.one("SELECT $1 AS value", 123)
+      .then(function (data) {
+        response.send('Hello From Heroku postGres DB ' + data.value);
+      })
+      .catch(function (error) {
+        response.send('Error From Heroku postGres DB ' + data.value);
+      });
+
 });
 
 app.listen(app.get('port'), function() {
